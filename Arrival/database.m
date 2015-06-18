b5075d4a1bcb419808f7342d83211c425abcfa78
Ben Zweig <benzilla@me.com>
2015-06-18 01:18:11 -0700

    pusg

--- Arrival/database.m
+++ Arrival/database.m
@@ -8,10 +8,23 @@
 
 #import "database.h"
 @import UIKit;
+@interface database ()
+@property (nonatomic, strong) UIView* collapsedView1;
+@property (nonatomic, strong) UIView* collapsedView2;
+@property (nonatomic, strong) UIView* expandedView1;
+@property (nonatomic, strong) UIView* expandedView2;
+@end
+
 
 @implementation database
 
 -(NSArray*)getViewArray{
+    self.collapsedView1 = self.collapsedView1 ? self.collapsedView1 : [UIView new];
+    self.collapsedView2 = self.collapsedView2 ? self.collapsedView2 : [UIView new];
+    self.expandedView1 = self.expandedView1 ? self.expandedView1 : [UIView new];
+    self.expandedView2 = self.expandedView2 ? self.expandedView2 : [UIView new];
+    
+    
     return @[
       @{
           @"collapsedView"    :   [UIView new],
