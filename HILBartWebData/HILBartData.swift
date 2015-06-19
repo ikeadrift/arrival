a970574a824d0ad3fba591bd4bf22b3702488a66
Bryce Dougherty <bryce.dougherty@gmail.com>
2015-06-19 00:46:59 -0700

    fixes

--- HILBartWebData/HILBartData.swift
+++ HILBartWebData/HILBartData.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+enum station: String {
+	case Rockridge = "ROCK"
+	case Powell = "POWL"
+	
+}
+
 public class HILBartData: NSObject {
+	
+	func getRouteInfo(origin: station, destination: station) {
+		let URLString = "http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=\(origin)&dest=\(destination)&date=now&time=now&key=MW9S-E7SL-26DU-VV8V"
+		
+		if let URL = NSURL(string: URLString) {
+		
+			let request = NSURLRequest(URL: URL)
+			
+			
+	
+		}
+	}
    
 }
